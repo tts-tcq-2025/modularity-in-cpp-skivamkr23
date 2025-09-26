421d8654a76ed8ae5bd7203712d7379490721fbe
Shivam <shivamkr02909@gmail.com>
2025-09-26 09:14:49 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,34 +1,23 @@
 #include <iostream>
 #include <assert.h>
-#include "ColorPair.hpp"
+#include "ColorMap.h"
+#include "ColorPair.h"
+#include "Test.h"
 
-void testNumberToPair(int pairNumber,
-    TelCoColorCoder::MajorColor expectedMajor,
-    TelCoColorCoder::MinorColor expectedMinor)
+namespace TelCoColorCoder 
 {
-    TelCoColorCoder::ColorPair colorPair =
-        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-    std::cout << "Got pair " << colorPair.ToString() << std::endl;
-    assert(colorPair.getMajor() == expectedMajor);
-    assert(colorPair.getMinor() == expectedMinor);
-}
+    void testNumberToPair(int pairNumber, MajorColor expectedMajor, MinorColor expectedMinor) 
+    {
+        ColorPair colorPair((MajorColor)((pairNumber - 1) / numberOfMinorColors),(MinorColor)((pairNumber - 1) % numberOfMinorColors));
+        std::cout << "Got pair " << colorPair.toString() << std::endl;
+        assert(colorPair.getMajorColor() == expectedMajor);
+        assert(colorPair.getMinorColor() == expectedMinor);
+    }
 
-void testPairToNumber(
-    TelCoColorCoder::MajorColor major,
-    TelCoColorCoder::MinorColor minor,
-    int expectedPairNumber)
-{
-    int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
-    std::cout << "Got pair number " << pairNumber << std::endl;
-    assert(pairNumber == expectedPairNumber);
-}
-
-int main() {
-    testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
-    testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
-
-    testPairToNumber(TelCoColorCoder::BLACK, TelCoColorCoder::ORANGE, 12);
-    testPairToNumber(TelCoColorCoder::VIOLET, TelCoColorCoder::SLATE, 25);
-
-    return 0;
+    void testPairToNumber(MajorColor major, MinorColor minor, int expectedPairNumber)
+    {
+        int pairNumber = (int)major * numberOfMinorColors + (int)minor + 1;
+        std::cout << "Got pair number " << pairNumber << std::endl;
+        assert(pairNumber == expectedPairNumber);
+    }
 }
