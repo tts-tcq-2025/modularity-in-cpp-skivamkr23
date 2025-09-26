42cbfeedb6fd56a0a7ed490aa29a4c27dc2e91a2
Shivam <shivamkr02909@gmail.com>
2025-09-26 09:10:40 +0530

    Update ColorPair.cpp

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -1,46 +1,21 @@
-#include "ColorPair.hpp"
-
-namespace TelCoColorCoder
+#include "ColorPair.h"
+namespace TelCoColorCoder 
 {
-    const char* MajorColorNames[] = {
-        "White", "Red", "Black", "Yellow", "Violet"
-    };
-    int numberOfMajorColors =
-        sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+    ColorPair::ColorPair(MajorColor major, MinorColor minor)
+        : majorColor(major), minorColor(minor) {}
 
-    const char* MinorColorNames[] = {
-        "Blue", "Orange", "Green", "Brown", "Slate"
-    };
-    int numberOfMinorColors =
-        sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-    ColorPair::ColorPair(MajorColor major, MinorColor minor):
-        majorColor(major), minorColor(minor)
-    {}
-
-    MajorColor ColorPair::getMajor() {
+    MajorColor ColorPair::getMajorColor() const 
+    {
         return majorColor;
     }
-    MinorColor ColorPair::getMinor() {
+
+    MinorColor ColorPair::getMinorColor() const 
+    {
         return minorColor;
     }
-    std::string ColorPair::ToString() {
-        std::string colorPairStr = MajorColorNames[majorColor];
-        colorPairStr += " ";
-        colorPairStr += MinorColorNames[minorColor];
-        return colorPairStr;
-    }
 
-    ColorPair GetColorFromPairNumber(int pairNumber) {
-        int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor =
-            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor =
-            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-        return ColorPair(majorColor, minorColor);
-    }
-
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major * numberOfMinorColors + minor + 1;
+    std::string ColorPair::toString() const 
+    {
+        return std::string(MajorColorNames[(int)majorColor]) + " " + MinorColorNames[(int)minorColor];
     }
 }
